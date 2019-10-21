b68164295802ea618dc56a2fa0e90b798ed5a932
Valentin Volkl <valentin.volkl@cern.ch>
2019-10-21 17:20:36 +0200

    up

--- hands-on/cpp/algo_par.cpp
+++ hands-on/cpp/algo_par.cpp
@@ -24,45 +24,49 @@ int main()
   std::generate_n(std::back_inserter(v), SIZE, [&]() { return uniform_dist(eng); });
 
   {
+    std::cout << "sum all the elements of the vector with std::accumulate" << std::endl;
     auto t0 = std::chrono::high_resolution_clock::now();
     // sum all the elements of the vector with std::accumulate
+    std::accumulate(v.begin(), v.end(), 0);
     auto t1 = std::chrono::high_resolution_clock::now();
     std::chrono::duration<float> d = t1 - t0;
-    std::cout << " in " << d.count() << " s\n";
+    std::cout << " \t in " << d.count() << " s\n";
   }
 
   {
+    std::cout << "sum all the elements of the vector with std::reduce, sequential policy" << std::endl;
     auto t0 = std::chrono::high_resolution_clock::now();
-    // sum all the elements of the vector with std::reduce, sequential policy
-    // NB you need to pass the initial value
+    double result = std::reduce(std::execution::sequenced_policy, v.begin(), v.end());
     auto t1 = std::chrono::high_resolution_clock::now();
     std::chrono::duration<float> d = t1 - t0;
-    std::cout << " in " << d.count() << " s\n";
+    std::cout << " \t in " << d.count() << " s\n";
   }
 
   {
+    std::cout << "sum all the elements of the vector with std::reduce, parallel policy" << std::endl;
     auto t0 = std::chrono::high_resolution_clock::now();
-    // sum all the elements of the vector with std::reduce, parallel policy
-    // NB you need to pass the initial value
+    double result = std::reduce(std::execution::par, v.begin(), v.end());
     auto t1 = std::chrono::high_resolution_clock::now();
     std::chrono::duration<float> d = t1 - t0;
-    std::cout << " in " << d.count() << " s\n";
+    std::cout << " \t in " << d.count() << " s\n";
   }
 
   {
+    std::cout << "sort the vector with std::sort " << std::endl;
     auto t0 = std::chrono::high_resolution_clock::now();
-    // sort the vector with std::sort
+    std::sort(v.begin(), v.end());
     auto t1 = std::chrono::high_resolution_clock::now();
     std::chrono::duration<float> d = t1 - t0;
-    std::cout << " in " << d.count() << " s\n";
+    std::cout << " \t in " << d.count() << " s\n";
   }
 
   {
+    std::cout << "sort the vector with std::sort, parallel policy" << std::endl;
     auto t0 = std::chrono::high_resolution_clock::now();
-    // sort the vector with std::sort, parallel policy
+    std::sort(std::execution::par, v.begin(), v.end());
     auto t1 = std::chrono::high_resolution_clock::now();
     std::chrono::duration<float> d = t1 - t0;
-    std::cout << " in " << d.count() << " s\n";
+    std::cout << " \t in " << d.count() << " s\n";
   }
 }
 
